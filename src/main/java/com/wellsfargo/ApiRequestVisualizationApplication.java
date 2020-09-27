a40002abdf66ccfb3ddccd68695c82a02c07c3d8
Aswin A <asweinnovate@gmail.com>
2020-09-28 00:28:44 +0530

    Added API requests visualization using HighCharts API

--- src/main/java/com/wellsfargo/ApiRequestVisualizationApplication.java
+++ src/main/java/com/wellsfargo/ApiRequestVisualizationApplication.java
@@ -2,12 +2,18 @@ package com.wellsfargo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class ApiRequestVisualizationApplication {
+public class ApiRequestVisualizationApplication extends WebMvcConfigurerAdapter{
+	
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/").setViewName("home");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiRequestVisualizationApplication.class, args);
 	}
-
 }
--- src/main/java/com/wellsfargo/controller/ApiRequestHitController.java
+++ src/main/java/com/wellsfargo/controller/ApiRequestHitController.java
@@ -1,64 +1,94 @@
 package com.wellsfargo.controller;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import com.wellsfargo.util.Constants;
 
-@RestController
+@Controller
 public class ApiRequestHitController {
 
-	public static Long total_count = 0l;
-	public static Long mobile_count = 0l;
-	public static Long web_count = 0l;
-	public static Long tab_count = 0l;
+	public static Integer total_count = 0;
+	public static Integer mobile_count = 0;
+	public static Integer web_count = 0;
+	public static Integer tab_count = 0;
 
 	@GetMapping("/mobile_app")
-	public String helloMobile() {
-		return updateCountsAndResult(Constants.MOBILE_APP);
+	public String helloMobile(Model model) {
+		updateCountsAndResult(Constants.MOBILE_APP);
+		Map<String, Integer> surveyMap = new LinkedHashMap<>();
+		surveyMap.put("mobile app", mobile_count);
+		surveyMap.put("web app", web_count);
+		surveyMap.put("tab app", tab_count);
+		model.addAttribute("surveyMap", surveyMap);
+		return "barGraph";
 	}
 
 	@GetMapping("/web_app")
-	public String helloWeb() {
-		return updateCountsAndResult(Constants.WEB_APP);
+	public String helloWeb(Model model) {
+		updateCountsAndResult(Constants.WEB_APP);
+		Map<String, Integer> surveyMap = new LinkedHashMap<>();
+		surveyMap.put("mobile app", mobile_count);
+		surveyMap.put("web app", web_count);
+		surveyMap.put("tab app", tab_count);
+		model.addAttribute("surveyMap", surveyMap);
+		return "barGraph";
 	}
-
+	
+    @RequestMapping("/foo")
+    @ResponseBody
+    public String handleFoo() {
+        return "home.html";
+    }
+	
 	@GetMapping("/tab_app")
-	public String helloTab() {
-		return updateCountsAndResult(Constants.TAB_APP);
+	public String barGraph(Model model) {
+		updateCountsAndResult(Constants.TAB_APP);
+		Map<String, Integer> surveyMap = new LinkedHashMap<>();
+		surveyMap.put("mobile app", mobile_count);
+		surveyMap.put("web app", web_count);
+		surveyMap.put("tab app", tab_count);
+		model.addAttribute("surveyMap", surveyMap);
+		return "barGraph";
 	}
 
-	public String updateCountsAndResult(int whoCalled) {
+	public void updateCountsAndResult(int whoCalled) {
 		total_count++;
-
 		String result;
 
 		switch (whoCalled) {
-			case Constants.MOBILE_APP:
-				synchronized (mobile_count) {
-					mobile_count++;
-				}
-				
-				//Enable the service requested through mobile
-				break;
-			case Constants.WEB_APP:
-				synchronized (mobile_count) {
-					web_count++;
-				}
-				
-				//Enable the service requested through web
-				break;
-			case Constants.TAB_APP:
-				synchronized (mobile_count) {
-					tab_count++;
-				}
-				//Enable the service requested through tab
-				break;				
-			default:
-				System.out.println("Error in API query");
+		case Constants.MOBILE_APP:
+			synchronized (mobile_count) {
+				mobile_count++;
+			}
+
+			// Enable the service requested through mobile
+			break;
+		case Constants.WEB_APP:
+			synchronized (mobile_count) {
+				web_count++;
+			}
+
+			// Enable the service requested through web
+			break;
+		case Constants.TAB_APP:
+			synchronized (mobile_count) {
+				tab_count++;
+			}
+			// Enable the service requested through tab
+			break;
+		default:
+			System.out.println("Error in API query");
 		}
-		result = "Mobile requests: "+mobile_count+"\n"+ "Web Requests: "+web_count+"\n"+"Tab Counts: "+
-				tab_count+"\n"+"Total counts: "+total_count;
-		return result;
+//		result = "Mobile requests: "+mobile_count+"\n"+ "Web Requests: "+web_count+"\n"+"Tab Counts: "+
+//				tab_count+"\n"+"Total counts: "+total_count;
+//		return result;
 	}
 
 }
